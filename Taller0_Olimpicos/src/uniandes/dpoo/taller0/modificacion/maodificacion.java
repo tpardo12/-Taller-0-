94fd2521ff7e76bfef0ee0cf6eff4c5eb0df03e5
tpardo12 <88736570+tpardo12@users.noreply.github.com>
2022-01-26 19:49:46 -0500

    Add files via upload

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/maodificacion.java
@@ -0,0 +1,18 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class maodificacion {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+
+	}
+
+}
